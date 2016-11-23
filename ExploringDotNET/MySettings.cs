da854ac207d9b9ec4796646b8b0b9e38f9332cb4
oadebiyi <tobi.adebiyi@kpmg.co.uk>
2016-11-23 13:49:29 +0000

    InitialCommit

---
+++ ExploringDotNET/MySettings.cs
@@ -0,0 +1,44 @@
+﻿namespace ExploringDotNET
+{
+    using System;
+    using System.IO;
+
+    internal class MySettings
+    {
+        public int MyNumber { get; set; }
+        public string MyString { get; set; }
+
+        public static void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static MySettings Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string SettingsFolder
+        {
+            get
+            {
+                var folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                folder = Path.Combine(folder, "MyCompany");
+                folder = Path.Combine(folder, "MyApp");
+
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                return folder;
+            }
+        }
+
+        private static MySettings DefaultSettings => new MySettings
+        {
+            MyNumber = 1,
+            MyString = ""
+        };
+    }
+}
